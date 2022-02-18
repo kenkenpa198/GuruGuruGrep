83c93999ffb5be3cd17250db82a374a9320935f6
kenkenpa198 <kenkenpa198@gmail.com>
2022-02-18 22:14:22 +0900

    [add][update]設定用ファイルや起動用バッチの実装。記述の整理。pptx 用関数の作成（途中）

--- GrepFiles.py
+++ GrepFiles.py
@@ -8,8 +8,10 @@ https://blog.honjala.net/entry/2016/06/24/005852
 
 import glob
 import os
+import re
 import sys
 
+import setup
 import utils
 
 print('\n========================')
@@ -17,10 +19,6 @@ print('       Grep Files       ')
 print('========================')
 
 try:
-
-    # 除外拡張子リストを指定
-    black_file_list = ['.doc', '.docx', '.xls', '.ppt', '.pptx']
-
     # 検索対象のディレクトリと検索するテキストを指定
     search_dir_input = input('\n検索対象のディレクトリパスを入力してください: ')
     search_dir = os.path.join(search_dir_input, '**')
@@ -30,50 +28,77 @@ try:
         print('\nGrep Files を終了します。\n')
         sys.exit()
 
-    search_txt = input('検索対象の文字列を入力してください: ')
+    search_txt = input('検索条件の文字列を入力してください: ')
 
-    print('\n入力された情報で検索を実行します。')
+    print('\n下記の検索条件で検索を実行します。')
+    print('----------------------------------------------------------\n')
+
+    print('検索ディレクトリ : ' + search_dir)
+    print('検索条件         : ' + search_txt)
+
+    if setup.DETECT_PATH:
+        print('検索ファイル     : ' + setup.DETECT_PATH)
+    else:
+        print('検索ファイル     : 設定なし')
+
+    if setup.EXCLUSION_PATH:
+        print('除外ファイル     : ' + setup.EXCLUSION_PATH)
+    else:
+        print('除外ファイル     : 設定なし')
+
+
+    print('\n----------------------------------------------------------\n')
+
     print('問題なければ Enter キーを押してください。')
     input('キャンセルする場合は Ctrl + C を押してください。')
 
-    # 指定ディレクトリのファイルをリスト化
-    file_path_list = [p for p in glob.glob(search_dir, recursive=True) if os.path.isfile(p)]
+    # 指定ディレクトリ以下に存在するファイルをリストへ格納する
+    file_path_list = [
+        p for p in glob.glob(search_dir, recursive=True) # 指定ディレクトリ以下に存在するファイルパスを再帰的に格納する
+        if re.search(setup.DETECT_PATH, p)               # 検索対象のファイルのみを格納する
+        if not re.search(setup.EXCLUSION_PATH, p)        # 除外対象のファイルは格納しない
+        if os.path.isfile(p)                             # 存在するファイルのみを格納する
+    ]
+
+    # for i in file_path_list:
+    #     print(i)
 
     print('\n▼ 検索結果')
     print('----------------------------------------------------------\n')
-    # 検索処理
-    for file_path in file_path_list:
 
+    # 検索処理
+    hit_num = 0
+    for file_path in file_path_list:
         # 拡張子によって条件分岐するための前処理
         root, ext = os.path.splitext(file_path)
 
         try:
-            # 除外拡張子リストに存在する場合は処理をスキップする
-            if ext in black_file_list:
-                continue
-
             # xlsx ファイルの場合の処理
             if ext == '.xlsx':
                 f_list = utils.read_xlsx_text(file_path)
-                utils.print_result(f_list, search_txt, file_path)
+                hit_num = utils.print_result(f_list, search_txt, file_path, hit_num)
                 continue
 
             # pptx ファイルの場合の処理
             if ext == '.pptx':
                 f_list = utils.read_pptx_text(file_path)
-                utils.print_result(f_list, search_txt, file_path)
+                hit_num = utils.print_result(f_list, search_txt, file_path, hit_num)
                 continue
 
-            # ファイルを開いて行ごとに検索
+            # Office 系でなかった場合はファイルを開いて検索する
             with open(file_path, encoding='utf-8') as f:
                 f_list = f.readlines()
-                utils.print_result(f_list, search_txt, file_path)
+                hit_num = utils.print_result(f_list, search_txt, file_path, hit_num)
                 continue
+
+        # ファイルが文字コードエラーで開けなかった場合はスキップする
         except UnicodeDecodeError as e:
             continue
             # print(utils.print_result_error(file_path, 'ファイルを読み込めませんでした。', e))
 
     print('\n----------------------------------------------------------')
+
+    print(f'{hit_num} 件ヒットしました。')
 
 except KeyboardInterrupt as e:
     print('\nキーボード入力により処理を中断しました。')
--- utils.py
+++ utils.py
@@ -1,52 +1,97 @@
-'''
-参考
-https://yuukou-exp.plus/handle-xlsx-with-python-intro/
-https://qiita.com/sino20023/items/0314438d397240e56576
-'''
-
+import glob
+import os
 import re
 import xml.etree.ElementTree as ET
 from zipfile import ZipFile
 
 
-# xlsx ファイルに書き込まれているテキストをリストにして返す関数
+'''
+■ .xlsx ファイルに書き込まれているテキストをリストにして返す関数
+
+.xlsx や .docx .pptx などの Office 系ファイルの実体は zip ファイルだそう。
+なので zip ファイルとして展開ができ、また展開されたディレクトリ内にはテキストや画像データが一式詰まっています。
+
+この特性を利用して、xlsx ファイルを zip アーカイブファイルとして読み込み、
+テキスト情報が詰まった xml ファイルからテキスト情報を抜き出してリスト形式で返すのがこの関数の役目です。
+pptx 用の関数も同じような感じです。
+
+テキスト情報が詰まった xml ファイルはツリー上のファイル形式なので、組み込みのモジュールを使って要素を抜き出しています。
+
+参考:
+https://yuukou-exp.plus/handle-xlsx-with-python-intro/
+https://qiita.com/sino20023/items/0314438d397240e56576
+https://qiita.com/mriho/items/f82c66e7a232b6b37206
+https://qiita.com/kaityo256/items/2977d53e70bbffd4d601
+'''
 def read_xlsx_text(src_file_path):
 
+    # zip アーカイブのオブジェクトを生成
+    zip = ZipFile(src_file_path)
+
     # xlsx ファイルの中に存在する xml ファイルの読み込み
-    zip = ZipFile(src_file_path)                     # ファイルを zip ファイルとして読み込む
-    xml_path = zip.open('xl/sharedStrings.xml', 'r') # zip ファイルの中に存在する xml ファイルを読み込む
-    tree = ET.parse(xml_path)                        # xml ファイルをパースされたオブジェクトとして読み込む
+    with zip.open('xl/sharedStrings.xml', 'r') as f:
+        tree = ET.parse(f)                           # zip ファイルの中に存在する xml ファイルをパースされたオブジェクトとして読み込む
     root = tree.getroot()                            # 親階層の要素を取り出す
 
-    # 親階層の子階層に存在するテキストをリストへ格納する
+    # 親階層から見て子階層に存在するテキストをリストへ格納する
     text_list = []
     for child in root:
         for child_2 in child:
             if child_2.text:
                 text_list.append(child_2.text)
-    return text_list
+    text_list_fmt = ['(改行)'.join(s.splitlines()) for s in text_list] # 改行が入っていると見づらいので改行を適当な文字列へ置換する
+    print(text_list)
+    print(text_list_fmt)
+    return text_list_fmt
 
 
-# pptx ファイルに書き込まれているテキストをリストにして返す関数
-# TODO: つくりかけ。
+'''
+■ .pptx ファイルに書き込まれているテキストを多次元リストにして返す関数（作成中）
+
+前述の関数のパワーポイント版。
+展開まではエクセル用と同じですが、パワーポイントの場合はスライドごとに xml ファイルが分かれているので
+スライドごとにテキストを格納した多次元リストとして返します。
+
+パワーポイント版関数のテキストの検知はエクセル版と違い正規表現のパターンマッチを利用しています。
+たぶんエクセル版と同じように子階層を掘っていくやり方でもできると思いますが、
+階層がエクセルよりも深いようで記述が冗長になりそうだったのでこちらを採用しました。
+'''
 def read_pptx_text(src_file_path):
 
-    # pptx ファイルの中に存在する xml ファイルの読み込み
-    zip = ZipFile(src_file_path)                       # ファイルを zip ファイルとして読み込む
-    xml_path = zip.open('ppt/slides/slide2.xml', 'r') # zip ファイルの中に存在する xml ファイルを読み込む
-    body = xml_path.read()
-    body = body.decode('utf-8')
-    print(body)
+    # zip アーカイブのオブジェクトを生成
+    zip = ZipFile(src_file_path)
 
-    # パターンマッチでテキストが格納されている箇所を検知してリスト化する
-    text_list = re.findall(r'\<a\:t\>.+?\<\/a\:t\>', body)
-    print(text_list)
-    return text_list
+    # ppt/slides/slideXX.xml をリストへ格納する
+    slide_path_list = [
+        zfp.filename for zfp in zip.filelist
+        if zfp.filename.startswith('ppt/slides/slide')
+    ]
+
+    print(slide_path_list)
+
+    # slideXX.xml ごとにリスト化を繰り返して多次元リストを作成
+    slide_text_list = []
+    for slide_path in slide_path_list:
+
+        # xml ファイルをデコードして読み込む
+        with zip.open(slide_path, 'r') as f:
+            body = f.read().decode('utf-8')
+
+        # パターンマッチでテキストが格納されている箇所を検知してリスト化する
+        text_list = re.findall(r'\<a\:t\>.+?\<\/a\:t\>', body)
+        text_list_fmt = [s.lstrip('<a:t>').rstrip('</a:t>') for s in text_list] # 左右のタグを削除する
+
+        slide_text_list.append(text_list_fmt)
+
+    print(slide_text_list)
+
+    return text_list_fmt
 
 
-
-# 検索して結果をプリントする関数
-def print_result(src_list, search_txt, file_path):
+'''
+■ 与えられた文字列をリスト上から検索して結果をプリントする関数
+'''
+def print_result(src_list, search_txt, file_path, hit_num):
     line_num = 1
     for line in src_list:
         m = re.search(search_txt, line)
@@ -54,9 +99,6 @@ def print_result(src_list, search_txt, file_path):
             # ファイルパス.txt (X行目, Y文字目) : 行のテキスト
             out = '%s (%d, %d) : %s' % (file_path, line_num, m.start() + 1, line.rstrip())
             print(out)
+            hit_num = hit_num + 1
         line_num = line_num + 1
-
-
-# 渡された内容をプリントする関数（例外用）
-def print_result_error(file_path, txt, error):
-    return '%s : %s <%s>' % (file_path, txt, error)
+    return hit_num
